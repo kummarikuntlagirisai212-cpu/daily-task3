33735059cb84f8b5b5ba779f212c7ee5ff063319
kummarikuntlagirisai212-cpu <kummarikuntlagirisai212@gmail.com>
2026-03-10 10:57:47 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,89 @@
+# 1. question
+glass_of_water= 2
+print("I drank", glass_of_water, "glasses of water today.") 
+# 2.question
+glass_of_water=3  
+glass_of_water=glass_of_water + 1  
+print(glass_of_water) 
+# 3.question
+men_stepped_on_the_moon= 1991 
+print( men_stepped_on_the_moon) 
+# 4.question
+my_reason_for_coding=  " to get a job"
+print( my_reason_for_coding) 
+# 5. question
+global_mean_sea_level_2018=21  
+global_mean_sea_level_2018= 22.1 
+print(global_mean_sea_level_2018)
+# 6. question
+staying_alive=  True
+print(staying_alive)
+# 7. question
+print(" 14,15,16,17")
+# 8.question
+
+my_text="Hello world" 
+print(my_text)
+# 9.question
+k="type here"
+s=k.replace("type here","Hello world")
+print(s)
+# 10. question
+A=True
+B=False
+print(A)
+print(B)
+
+
+# 11.question
+x=100
+print(x)
+
+
+
+#13. You are reading Python code, and these statements appear scattered in 
+#different locations throughout the code: 
+#employeenumber = 4398  
+ 
+  
+#EmployeeNumber = 4398  
+ 
+ 
+#employeeNumber = 4398 
+#A. These statements refer to different variables. 
+#B. These statements refer to the same variable.
+# answer  A is different 
+
+
+#15.question
+a=5*2**10
+print(a)
+
+
+
+
+
+
+
+
+
+#12. Which of the following statements assigns the value 100 to the variable x 
+#in Python: 
+#A. x := 100 
+#B. let x = 100 
+#C. x << 100 
+#D. x = 100 
+#E. x ← 100 
+#answers
+#A
+#B
+#C
+
+#14.  Which of the following would give a syntax error? 
+#A. x, y, z = 10, 20, 30 
+#B. x, y, z = 10, ‘Hello’, True 
+#C. x = 10, y =’Hello’, z = True 
+#D. None of the above 
+
+#answer
+#c
